077813fb0408d72981ca3b15e42f4fd8b08da6e1
Rafael Gabardo <rggabardo@gmail.com>
2023-06-20 16:03:58 -0300

    Facilitando a visualização

---
+++ Site/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <title>Index</title>
+</head>
+<body>
+    <nav class="nav">
+        <section class="section-nav">
+            <div class="empty">
+
+            </div>
+            <div class="div-nav">
+                <a href="https://www.instagram.com/gusgabardo/?hl=pt-br" target="_blank">
+                    <img class="img-nav" src="Imagem-Gus-Nav.jpg" alt="Gustavo tocando">
+                </a>
+            </div>
+            <div class="div-nav">
+                <div class="div-teste">
+                    <ul class="list">
+                        <div class="div-card">
+                            <li>
+                                <a href="">Início</a>
+                            </li>
+                        </div>
+                        <div class="empty2">
+
+                        </div>
+                        <div class="div-card">
+                            <li>
+                                <a href="">Portfólio</a>
+                            </li>
+                        </div>
+                        <div class="empty2">
+
+                        </div>
+                        <div class="div-card">
+                            <li>
+                                <a href="">Contato</a>
+                            </li>
+                        </div>
+                        <div class="empty2">
+
+                        </div>
+                    </ul>
+                </div>
+            </div>
+        </section>
+    </nav>
+</body>
+</html>

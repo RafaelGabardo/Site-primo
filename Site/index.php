729f52d89aa4e3f4083cea543a3cc2ee5ed0a2d6
Rafael Gabardo <rggabardo@gmail.com>
2023-06-21 11:03:53 -0300

    Adições no index

--- Site/index.php
+++ Site/index.php
@@ -22,7 +22,7 @@
                     <ul class="list">
                         <div class="div-card">
                             <li>
-                                <a href="index.html">Início</a>
+                                <a href="index.php">Início</a>
                             </li>
                         </div>
                         <div class="empty2">
@@ -30,7 +30,7 @@
                         </div>
                         <div class="div-card">
                             <li>
-                                <a href="portfolio.html">Portfólio</a>
+                                <a href="portfolio.php">Portfólio</a>
                             </li>
                         </div>
                         <div class="empty2">
@@ -38,7 +38,7 @@
                         </div>
                         <div class="div-card">
                             <li>
-                                <a href="contato.html">Contato</a>
+                                <a href="contato.php">Contato</a>
                             </li>
                         </div>
                         <div class="empty2">
@@ -52,9 +52,20 @@
     <header class="header">
         <section class="section section-header">
             <div class="div-header">
-
+                
             </div>
         </section>
     </header>
+    <main class="main">
+        <section class="section section-main">
+            <div class="div-main">
+                <img class="img-main" src="Imagem-Gus-Main.jpg" alt="Gustavo no concerto do Deep Purple">
+            </div>
+            <div class="div-main">
+                <h1>Biografia</h1>
+                <p></p>
+            </div>
+        </section>
+    </main>
 </body>
 </html>
